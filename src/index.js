fa044287474263f304b5122611d082aa14ce2532
hsz <jakub@chrzanowski.info>
2018-12-11 07:00:36 +0100

    use `Global` from emotion package for global styling

--- src/index.js
+++ src/index.js
@@ -1,20 +1,20 @@
-/* eslint-disable */
+import { Global } from '@emotion/core';
 import { ConnectedRouter } from 'connected-react-router/immutable';
-import { Authorize, Login } from 'containers';
+import { Login } from 'containers';
 import App from 'containers/App';
-import { injectGlobal } from 'emotion';
 import { render } from 'react-dom';
 import { LocalizeProvider } from 'react-localize-redux';
 import { Provider } from 'react-redux';
 import { Route, Switch } from 'react-router-dom';
 import createStore, { history } from 'store';
-import { React } from 'utils/create';
+import { css, React } from 'utils/create';
 import registerServiceWorker from './registerServiceWorker';
 
-injectGlobal`
+const globalStyles = css`
   body {
-    background: #eceef1 url('/stripes.svg')
+    background: #eceef1 url('/stripes.svg');
   }
+
   #root {
     min-height: 100%;
     display: flex;
@@ -26,10 +26,11 @@ const store = createStore();
 render(
   <Provider store={store}>
     <LocalizeProvider>
+      <Global styles={globalStyles} />
       <ConnectedRouter history={history}>
         <Switch>
           <Route exact component={Login} path="/(auth|login)" />
-          <Route component={App} path="/" />
+          <Route component={App} />
         </Switch>
       </ConnectedRouter>
     </LocalizeProvider>
