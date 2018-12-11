392d1014cae7fad61fb8d53b7cf27b5c9fa3cfc2
hsz <jakub@chrzanowski.info>
2018-12-11 07:20:11 +0100

    integrate with hard-source-webpack-plugin (finally fixed)

--- config-overrides.js
+++ config-overrides.js
@@ -2,6 +2,7 @@ const { compose, injectBabelPlugin } = require('react-app-rewired');
 const rewireESLint = require('react-app-rewire-eslint');
 const rewireYAML = require('react-app-rewire-yaml');
 const rewireLess = require('react-app-rewire-less');
+const HardSourceWebpackPlugin = require('hard-source-webpack-plugin');
 
 module.exports = compose(
   rewireYAML,
@@ -11,6 +12,7 @@ module.exports = compose(
     ),
   config => injectBabelPlugin(['emotion', { sourceMap: true, autoLabel: true }], config),
   config => injectBabelPlugin(['import', { libraryName: 'antd', style: true }], config),
+  config => ({ ...config, plugins: config.plugins.concat(new HardSourceWebpackPlugin()) }),
   rewireLess.withLoaderOptions({
     javascriptEnabled: true,
     modifyVars: {
