9af114e726475ac5edc85954d9c874892155fcb4
hsz <jakub@chrzanowski.info>
2019-01-21 08:33:55 +0100

    redux-saga - initialize app when SPACES_LIST and PROFILE_GET are fulfilled

--- src/sagas/index.js
+++ src/sagas/index.js
@@ -3,8 +3,8 @@ import { PROFILE_GET } from 'reducers/auth/types';
 import { fetchCollections } from 'reducers/collections/actions';
 import { COLLECTIONS_DELETE, COLLECTIONS_UPDATE } from 'reducers/collections/types';
 import { setSpace } from 'reducers/spaces/actions';
-import { SET_SPACE } from 'reducers/spaces/types';
-import { all, put, takeLatest } from 'redux-saga/effects';
+import { SET_SPACE, SPACES_LIST } from 'reducers/spaces/types';
+import { all, fork, put, take, takeEvery } from 'redux-saga/effects';
 
 const APP = 'APP';
 const pending = action => `${action}_PENDING`;
@@ -24,22 +24,30 @@ function* preInitializeApp() {
   yield put({ type: pending(APP) });
 }
 
-function* initializeApp({
-  payload: {
-    data: { user_metadata: { space } = {} },
-  },
-}) {
+function* initializeApp() {
+  const [
+    ,
+    {
+      payload: {
+        data: { user_metadata: { space } = {} },
+      },
+    },
+  ] = yield all([take(fulfilled(SPACES_LIST)), take(fulfilled(PROFILE_GET))]);
+
   yield put(setSpace(space));
   yield put({ type: fulfilled(APP) });
 }
 
 // eslint-disable-next-line no-unused-vars
 function* rootSaga(getState) {
-  yield all([takeLatest(SET_SPACE, handleSetSpace)]);
-  yield all([takeLatest(fulfilled(COLLECTIONS_DELETE), refreshCollections)]);
-  yield all([takeLatest(fulfilled(COLLECTIONS_UPDATE), refreshCollections)]);
-  yield all([takeLatest(pending(PROFILE_GET), preInitializeApp)]);
-  yield all([takeLatest(fulfilled(PROFILE_GET), initializeApp)]);
+  yield all([
+    takeEvery(SET_SPACE, handleSetSpace),
+    takeEvery(fulfilled(COLLECTIONS_DELETE), refreshCollections),
+    takeEvery(fulfilled(COLLECTIONS_UPDATE), refreshCollections),
+    takeEvery(pending(PROFILE_GET), preInitializeApp),
+  ]);
+
+  yield fork(initializeApp);
 }
 
 export default rootSaga;
